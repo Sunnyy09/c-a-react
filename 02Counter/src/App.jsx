ed46f997cf76dabe8bc071f544add75380d5bec9
Sunnyy09 <12001109@csepup.ac.in>
2024-11-13 08:16:38 +0530

    update file!

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,36 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
-  let [counter, setCounter] = useState(1) //Counter is a value at 0th index, setCounter: is a function
+  let [counter, setCounter] = useState(1); //Counter is a value at 0th index, setCounter: is a function
 
   //let counter = 5;
 
   const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter)
-    console.log("Clicked", counter)
-  }
+    // if (counter < 20) {
+    //   counter = counter + 1;
+    //   setCounter(counter);
+    // }
 
-  const removeValue = () => {
-    counter = counter - 1;
-    setCounter(counter)
-    //console.log("Clicked", counter)
-  }
-  
+    setCounter((prevCounter) => (prevCounter < 20 ? prevCounter + 1 : 0));
+  };
+
+  const subtractValue = () => {
+    // if (counter > 0) {
+    //   counter = counter - 1;
+    //   setCounter(counter);
+    // }
+    //Another method to subtract a value in a cleaner way
+    setCounter((prevCounter) => (prevCounter > 0 ? prevCounter - 1 : 0));
+  };
+
   return (
     <>
-    <h1>Count the Counter</h1>
-    <h2>Counter Value: {counter}</h2>
+      <h1>Count the Counter</h1>
+      <h2>Counter Value: {counter}</h2>
 
-    <button onClick={addValue }>Add Value</button>
-    <br />
-    <button onClick={removeValue}>Remove Value</button>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
