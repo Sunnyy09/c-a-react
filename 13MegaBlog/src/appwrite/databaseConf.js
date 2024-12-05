d74a6520683fb37e0b715cf4e129c45385c2dddc
Sunnyy09 <12001109@csepup.ac.in>
2024-12-05 19:21:41 +0530

    add another file service

--- 13MegaBlog/src/appwrite/databaseConf.js
+++ 13MegaBlog/src/appwrite/databaseConf.js
@@ -91,6 +91,8 @@ export class DbService {
     }
   }
 
+  //file services
+
   async uploadFile(file) {
     try {
       return await this.bucket.createFile(
@@ -112,6 +114,10 @@ export class DbService {
       throw error;
       return false;
     }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(config.appwriteBucketId, fileId);
   }
 }
 
